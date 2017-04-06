71456ad90eec3aa15de6668de8917f45663f056f
Ernesto <ramoscaciqueernesto@hotmail.com>
2017-04-06 14:55:07 -0500

    First commit

---
+++ debug/sources/freertos_queue.d
@@ -0,0 +1,75 @@
+sources/freertos_queue.o: ../sources/freertos_queue.c \
+ ../freertos/Source/include/FreeRTOS.h ../sources/FreeRTOSConfig.h \
+ ../freertos/Source/include/projdefs.h \
+ ../freertos/Source/include/portable.h \
+ ../freertos/Source/include/deprecated_definitions.h \
+ ../freertos/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ ../freertos/Source/include/mpu_wrappers.h \
+ ../freertos/Source/include/task.h ../freertos/Source/include/list.h \
+ ../freertos/Source/include/queue.h ../freertos/Source/include/timers.h \
+ ../freertos/Source/include/task.h ../devices/fsl_device_registers.h \
+ ../devices/MK64F12.h ../CMSIS/Include/core_cm4.h \
+ ../CMSIS/Include/core_cmInstr.h ../CMSIS/Include/cmsis_gcc.h \
+ ../CMSIS/Include/core_cmFunc.h ../CMSIS/Include/core_cmSimd.h \
+ ../devices/system_MK64F12.h ../devices/MK64F12_features.h \
+ ../utilities/fsl_debug_console.h ../drivers/fsl_common.h \
+ ../drivers/fsl_clock.h ../drivers/fsl_common.h ../sources/board.h \
+ ../sources/clock_config.h ../drivers/fsl_gpio.h ../sources/pin_mux.h
+
+../freertos/Source/include/FreeRTOS.h:
+
+../sources/FreeRTOSConfig.h:
+
+../freertos/Source/include/projdefs.h:
+
+../freertos/Source/include/portable.h:
+
+../freertos/Source/include/deprecated_definitions.h:
+
+../freertos/Source/portable/GCC/ARM_CM4F/portmacro.h:
+
+../freertos/Source/include/mpu_wrappers.h:
+
+../freertos/Source/include/task.h:
+
+../freertos/Source/include/list.h:
+
+../freertos/Source/include/queue.h:
+
+../freertos/Source/include/timers.h:
+
+../freertos/Source/include/task.h:
+
+../devices/fsl_device_registers.h:
+
+../devices/MK64F12.h:
+
+../CMSIS/Include/core_cm4.h:
+
+../CMSIS/Include/core_cmInstr.h:
+
+../CMSIS/Include/cmsis_gcc.h:
+
+../CMSIS/Include/core_cmFunc.h:
+
+../CMSIS/Include/core_cmSimd.h:
+
+../devices/system_MK64F12.h:
+
+../devices/MK64F12_features.h:
+
+../utilities/fsl_debug_console.h:
+
+../drivers/fsl_common.h:
+
+../drivers/fsl_clock.h:
+
+../drivers/fsl_common.h:
+
+../sources/board.h:
+
+../sources/clock_config.h:
+
+../drivers/fsl_gpio.h:
+
+../sources/pin_mux.h:
